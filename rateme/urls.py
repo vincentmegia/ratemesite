4417e1492319bbfde6cc25b2e8d62d7dcf3b1a46
vincentmegia <vincent.megia@gmail.com>
2014-02-02 09:47:15 -0500

    updates

--- rateme/urls.py
+++ rateme/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
-from ratemesite.rateme.views import supplierview
+from rateme.views import supplierview
 
 urlpatterns = patterns('',
     url(r'^$', supplierview),
-)
+)  
