824a82d24692ba8c79d76f1e4774d47308995e7f
vincentmegia <vincent.megia@gmail.comz>
2014-05-01 10:08:09 +0800

    updates

--- rateme/utilities/JSONSerializer.py
+++ rateme/utilities/JSONSerializer.py
@@ -1,11 +1,4 @@
 __author__ = 'bastardo'
 
-from django.core.serializers.json import Serializer
-
-class JSONSerializer(Serializer):
-
-    def end_serialization(self):
-        for ob in self.objects:
-            del ob['pk']
-            del ob['model']
-        return super(JSONSerializer, self).end_serialization()
+class a:
+    pass
